7fe3efc9bcede6cb4b4ea18b609ab9982ed7f44f
rushi.jagdale <rjcom99@gmail.com>
2021-06-05 09:26:13 +0530

    add

--- CovidBed/settings.py
+++ CovidBed/settings.py
@@ -75,14 +75,19 @@ WSGI_APPLICATION = 'CovidBed.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'Hospital',
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
